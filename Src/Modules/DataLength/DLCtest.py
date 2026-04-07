4ede051bb9d07946550c4424a32fe0c618b42d85
moeab77 <moeab@umich.edu>
2026-04-07 14:40:30 -0400

    Line number added to issue

--- Src/Modules/DataLength/DLCtest.py
+++ Src/Modules/DataLength/DLCtest.py
@@ -2,22 +2,97 @@ import os
 from sys import argv
 import dlc_analyzer
 
+import tree_sitter as TreeSitter
+import tree_sitter_cpp as _CPP
+CPP_LANGUAGE = TreeSitter.Language(_CPP.language())
 
-FOLDER = "/Users/abrahamabdulkarim/Documents/code/CAN_bus_research/Src/Modules/DataLength/Test_Cases/"
-#/Users/moeab/CAN_bus_research/Src/Modules/DataLength/Test_Cases/
+### USAGE #######################################################################
+
+# Run in terminal.
+#   argument 1 (argv[1]) is mode, use one of the following
+#   - testAll: Runs through all test cases in the Test_Cases folder
+#   - testFolder: Tests the test case files in the specified child folder of Test_Cases
+#   - testOne: Tests the specified file for the test case.
+#
+#   argument 2 (argv[2]) is path
+#   - Should be path to parent folder or specific file, depending on mode
+#
+#################################################################################
+
+### FILE PATH TO THE GITHUB FOLDER TITLED 'DataLength'                        ###
+### Should be something along the line of:                                    ###
+### {SAVE_LOCATION}/CAN-Static-AnalysisSrc/AnalysisSrc/DataLength/Test_Cases/ ###
+
+#################################################################################
+
+MODE = argv[1]
+PATH = argv[2]
+
 analyzer = dlc_analyzer.DLCAnalyzer()
 
-for item in os.listdir(FOLDER):
-    if(item[:5] == "test_"):
-        path1 = FOLDER + item
-        print('_' * 100)
-        print(f'Testing {item[5:]}\n')
-       
-        for file in sorted(os.listdir(path1)):
-            if(file[-4:] == '.ino' or file[-4:] == '.cpp'):
-                print(f'Test: {file}')
-                path2 = path1 + '/' + file
-                analyzer.checkDLC(path2)
-                print()
+def testAll():
+    for item in os.listdir(PATH):
+        if(item[:5] == "test_"):
+            path1 = PATH + item
+            print('_'*100)
+            print(f'Testing {item[5:]}\n')
+            for file in os.listdir(path1):
+                if(file[-4:] == '.ino' or file[-4:] == '.cpp'):
+                    print(f'Test: {file}')
+                    path2 = path1 + '/' + file
+
+                    with(open(path2, 'r', encoding='utf-8') as inFile):
+                        sourceCode = inFile.read()
+
+                    parser = TreeSitter.Parser(CPP_LANGUAGE)
+                    tree = parser.parse(bytes(sourceCode, "utf8"))
+                    root = tree.root_node
+
+                    issues, messages, line_nums = analyzer.checkDLC(root)
+                    print()
+
+def testFolder(folderPath):
+
+    print('_'*100)
+    for file in os.listdir(folderPath):
+        if(file[-4:] == '.ino' or file[-4:] == '.cpp'):
+            print(f'Test: {file}')
+            path2 = folderPath + '/' + file
+
+            with(open(path2, 'r', encoding='utf-8') as inFile):
+                sourceCode = inFile.read()
+
+            parser = TreeSitter.Parser(CPP_LANGUAGE)
+            tree = parser.parse(bytes(sourceCode, "utf8"))
+            root = tree.root_node
+
+            issues, messages, line_nums = analyzer.checkDLC(root)
+            print()
+
+def testOne(filepath):
+    with(open(filepath, 'r', encoding='utf-8') as inFile):
+        sourceCode = inFile.read()
+
+    parser = TreeSitter.Parser(CPP_LANGUAGE)
+    tree = parser.parse(bytes(sourceCode, "utf8"))
+    root = tree.root_node
+
+    issues, messages, line_nums = analyzer.checkDLC(root)
+    print()
+
+###########################################################################################################################################################
+
+if not (os.path.exists(PATH)):
+    print("Please enter a valid file path.")
+    #exit(0)
+
+if(MODE.lower() == "testall"):
+    testAll()
+elif(MODE.lower() == "testfolder"):
+    testFolder(PATH)
+elif(MODE.lower() == "testone"):
+    testOne(PATH)
+else:
+    print("Please enter a valid mode.")
 
         
